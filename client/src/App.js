6f1e59233cf64eafe0bedf930d16b5714a377a49
Franco <phanfranco@yahoo.com>
2021-09-09 14:48:16 -0700

    Refactored app and landing to look for readible and clean.

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { useEffect, useState } from "react";
 import {
   getCurrentWeatherBySearch,
@@ -10,8 +10,6 @@ import LandingPage from "./pages/LandingPage";
 import Video from "./globalComponents/Video";
 import landingVideo from "./assets/LandingPage.mp4";
 import Nav from "./globalComponents/Nav";
-import LandingText from "./landingComponents/LandingText";
-import LandingButton from "./landingComponents/LandingButton";
 import Home from "./pages/Home";
 import SideBar from "./homeComponents/Sidebar";
 import Body from "./homeComponents/Body";
@@ -47,14 +45,7 @@ function App() {
       <Video src={landingVideo} />
       <Nav />
       <Switch>
-        <Route exact path="/">
-          <LandingPage>
-            <LandingText>Welcome to my Weather App</LandingText>
-            <Link to="/home">
-              <LandingButton>Get Started</LandingButton>
-            </Link>
-          </LandingPage>
-        </Route>
+        <Route exact path="/" component={LandingPage} />
         <Route path="/home">
           {/* <Video src={landingVideo} />
           <Nav /> */}
--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,5 +1,8 @@
 import React from "react";
 import styled from "styled-components";
+import LandingText from "../landingComponents/LandingText";
+import LandingButton from "../landingComponents/LandingButton";
+import { Link } from "react-router-dom";
 
 const LandingPageContainer = styled.div`
   margin: 0;
@@ -22,10 +25,15 @@ const LandingWelcome = styled.div`
   align-content: center;
 `;
 
-const LandingPage = ({ children }) => {
+const LandingPage = () => {
   return (
     <LandingPageContainer>
-      <LandingWelcome>{children}</LandingWelcome>
+      <LandingWelcome>
+        <LandingText>Welcome to my Weather App</LandingText>
+        <Link to="/home">
+          <LandingButton>Get Started</LandingButton>
+        </Link>
+      </LandingWelcome>
     </LandingPageContainer>
   );
 };
